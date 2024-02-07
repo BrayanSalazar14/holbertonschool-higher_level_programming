2f62039ddb508eeab44c2f29512172391f231440
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-07 17:09:43 -0500

    hot fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    name = dir(hidden_4) 
+    name = dir(hidden_4)
     for i in range(len(name)):
         if name[i][:2] != "__":
             print("{}".format(name[i]))
