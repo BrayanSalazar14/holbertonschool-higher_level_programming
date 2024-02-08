f0b754e450ad74dcebecebc5dbe4c4f8e7a9047e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-08 11:10:45 -0500

    new task V1

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    myStringCopy = [] 
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            myStringCopy.append(i)
+    myNewString = ''.join(myStringCopy)
+    return myNewString
