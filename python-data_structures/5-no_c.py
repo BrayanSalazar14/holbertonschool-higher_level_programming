0093c196735873b7e2419c95b39d388a27bc29d1
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-09 00:28:31 -0500

    new methods

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    """First method with append to insert elements and .join to cancatenate
     myStringCopy = []
     for i in my_string:
         if i != 'c' and i != 'C':
             myStringCopy.append(i)
     myNewString = ''.join(myStringCopy)
     return myNewString
+    """
+    """Second Method concatenated strings"""
+    strCpy = ""
+    for chars in my_string:
+        if chars != 'c' and chars != 'C':
+            strCpy += chars
+    return strCpy
