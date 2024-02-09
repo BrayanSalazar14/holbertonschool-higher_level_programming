1239b1eeb2ec5fd868f417ee262e9e59894171cf
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-08 23:57:28 -0500

    New forms

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,6 +3,16 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
 
+    """ Con Metodo sort
     length = len(my_list)
     my_list.sort()
     return my_list[length - 1]
+    """
+
+    """Sin metodo sort"""
+    length = len(my_list)
+    numberMax = my_list[0]
+    for index in my_list:
+        if index > numberMax:
+            numberMax = index
+    return numberMax
