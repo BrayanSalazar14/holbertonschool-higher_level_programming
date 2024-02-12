34234729de87427b6f46e708a1c1be5145f3387b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 12:25:08 -0500

    new task

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
+
+
+"""Other form
+def common_elements(set_1, set_2):
+    common_set = set()
+    for elem in set_1:
+        if elem in set_2:
+            common_set.add(elem)
+    return common_set
+"""
