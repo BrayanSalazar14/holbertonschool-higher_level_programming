c1da09f78078675817b458d16e8e664c03b5f44d
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 15:30:33 -0500

    V4

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
+"""def update_dictionary(a_dictionary, key, value):
     for index in range(len(a_dictionary)):
         a_dictionary[key] = value
+    return a_dictionary"""
+
+def update_dictionary(a_dictionary, key, value):
+    for index in range(len(a_dictionary)):
+        if key in a_dictionary:
+            a_dictionary[key] = value
+        else:
+            a_dictionary[key] = value
     return a_dictionary
