03491cb501b0e8fe9b72a1f7a5a317570861fcd7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 15:10:44 -0500

    pycodestyle

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 def update_dictionary(a_dictionary, key, value):
     for index in range(len(a_dictionary)):
         if index != key:
-            a_dictionary[key] = value 
+            a_dictionary[key] = value
     return a_dictionary
