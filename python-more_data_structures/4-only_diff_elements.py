c44f4ce77b46bce6e91f092b6452fff46c86c0a0
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 13:08:57 -0500

    Other form add

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,14 +3,7 @@ def only_diff_elements(set_1, set_2):
     return set_1.symmetric_difference(set_2)
 
 
-"""
+"""Other form
 def only_diff_elements(set_1, set_2):
-    newSet = set()
-    commondElem = set_1.intersection(set_2)
-    for elem in set_1:
-        for elem2 in set_2:
-            if elem not in commondElem and elem2 not in commondElem:
-                newSet.add(elem)
-                newSet.add(elem2)
-    return newSet
+    return set_1 ^ set_2
 """
