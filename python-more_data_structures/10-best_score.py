d4081aa4fc778b77356724d043c849e3f85ec55e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 17:58:11 -0500

    Other form for application algorithm

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,3 +3,15 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return None
     return max(a_dictionary, key=a_dictionary.get)
+
+
+"""Other form
+def best_score(a_dictionary):
+    if a_dictionary is None or not a_dictionary:
+        return None
+
+    bestScore = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == bestScore:
+            return key
+"""
