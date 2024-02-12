fd983f535d2777d6735bc883a8adc4aef4c507c7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-12 15:18:03 -0500

    V2

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     for index in range(len(a_dictionary)):
-        if index != key:
+        if a_dictionary != key:
             a_dictionary[key] = value
+        else:
+            a_dictionary[index] = value
     return a_dictionary
