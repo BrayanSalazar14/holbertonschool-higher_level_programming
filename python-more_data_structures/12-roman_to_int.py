7b7c5b7b26155f254f1daa691e673bd8663bccab
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 00:07:12 -0500

    V2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return None
 
-    acronym = dict([
-        ("I", 1), ("V", 5), ("X", 10),
-        ("L", 50), ("C", 100), ("D", 500), ("M", 1000)
-        ])
+    acronym = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     prev_value = 0
     for symbol in reversed(roman_string):
