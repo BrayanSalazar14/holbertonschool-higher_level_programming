5530f1d0036a81a6d12ab4a3152764cb2c7ef6d2
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 11:56:13 -0500

    V2

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         check = int(value)
-        print("{:d}".format(value))
+        print("{:d}".format(check))
         return True
     except ValueError:
         return False
