0a01a8ee330001de31bf99f3dac1a3f673ffca65
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 14:58:52 -0500

    VF

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,6 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         except (TypeError, ValueError):
+            div = 0
+            newList.append(div)
             print("wrong type")
         finally:
             pass
