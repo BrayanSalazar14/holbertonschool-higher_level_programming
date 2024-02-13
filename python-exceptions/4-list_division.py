663c114fd39dc410107b6f770d43bfb35899215c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 15:01:15 -0500

    VF

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,6 +11,8 @@ def list_division(my_list_1, my_list_2, list_length):
             newList.append(div)
             print("division by 0")
         except IndexError:
+            div = 0
+            newList.append(div)
             print("out of range")
         except (TypeError, ValueError):
             div = 0
