c12f73f1b320d66e9812da5182fdb0a22b8b22c4
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 12:05:04 -0500

    VF and exepcion add typeError

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         check = int(value)
-        print("{:d}".format(check))
-    except ValueError:
-        return False
-    else:
+        print("{:d}".format(value))
         return True
+    except (TypeError, ValueError):
+        return False
