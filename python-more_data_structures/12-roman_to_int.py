7c23e74a8314354b0afd3b31cbc5076428f9ebed
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 00:52:53 -0500

    other form

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,6 +8,32 @@ def roman_to_int(roman_string):
         ("I", 1), ("V", 5), ("X", 10),
         ("L", 50), ("C", 100), ("D", 500), ("M", 1000)
         ])
+
+    container = []
+    for symbol in roman_string:
+        container.append(acronym[symbol])
+
+    for index in range(len(container) - 1):
+        if container[index] < container[index + 1]:
+            container[index] = container[index] * - 1
+
+    total = 0
+    for index in container:
+        total += index
+
+    return total
+
+
+"""Other form
+def roman_to_int(roman_string):
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    acronym = dict([
+        ("I", 1), ("V", 5), ("X", 10),
+        ("L", 50), ("C", 100), ("D", 500), ("M", 1000)
+        ])
     total = 0
     prev_value = 0
     for symbol in reversed(roman_string):
@@ -17,4 +43,4 @@ def roman_to_int(roman_string):
         else:
             total += value
         prev_value = value
-    return total
+    return total"""
