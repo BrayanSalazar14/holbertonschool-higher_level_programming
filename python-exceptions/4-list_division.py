45283025b17a1de2aaec43d106e60a98e79070bf
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-13 14:53:44 -0500

    new task V1

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+def list_division(my_list_1, my_list_2, list_length):
+
+    newList = []
+    for index in range(list_length):
+        try:
+            div = my_list_1[index] / my_list_2[index]
+            newList.append(div)
+        except ZeroDivisionError:
+            div = 0
+            newList.append(div)
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except (TypeError, ValueError):
+            print("wrong type")
+    return newList
