4c823dfbbcda8028731859f1b3d085a5ca3976a4
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-16 13:43:39 -0500

    new task V1

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This is the "print_square" module.
+The add module supplies one function, print_square().  For example,
+>>> print_square(2)
+##
+##
+"""
+
+
+def print_square(size):
+    """Function that prints a square
+    Print a square using size
+    """
+    if isinstance(size, int) and size >= 0:
+        if size >= 0:
+            for index in range(size):
+                print("#" * size)
+        else:
+            raise ValueError("size must be >= 0")
+    else:
+        raise TypeError("size must be an integer")
+
+
+"""Other form"""
+"""def print_square(size):"""
+#    if isinstance(size, int) and size >= 0:
+#        if size >= 0:
+#            for rows in range(size):
+#                for columns in range(size):
+#                    print("#", end="")
+#               print()
+#       else:
+#           raise ValueError("size must be >= 0")
+#   else:
+#       raise TypeError("size must be an integer")
