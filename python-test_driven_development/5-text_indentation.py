055af1b26fd5014028dcbadac95d985009df29ae
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-16 15:16:40 -0500

    new task v1

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is the "text_indentation" module.
+The add module supplies one function, text_indentation().  For example,
+>>> text_indentation("hi.")
+hi.
+
+"""
+
+
+def text_indentation(text):
+    """Function that prints an text
+    function that prints a text with 2 new lines after each of
+    these characters: ., ? and :
+    """
+    textIndent = ""
+    if isinstance(text, str):
+        for chars in text:
+            if chars != '.' and chars != '?' and chars != ':':
+                textIndent += chars
+            else:
+                textIndent += chars
+                textIndent += "\n" * 2
+        print(textIndent)
+    else:
+        raise TypeError("text must be a string")
