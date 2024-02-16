f6edede96647a3b4a6f459bb505c320c48b9f604
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-16 11:16:47 -0500

    documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This is the "say_my_name" module.
+The add module supplies one function, say_my_name().  For example,
+>>> say_my_name("Brayan", Salazar)
+My name is Brayan Salazar
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Function with two parameters
+    print the string complete
+    """
     if isinstance(first_name, str):
         if isinstance(last_name, str):
             print("My name is {} {}".format(first_name, last_name))
