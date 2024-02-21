04490de162917fbb7786a9c92ebd564ea8311d02
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-21 10:13:37 -0500

    Doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""Funcion that returns the list of available attributes and methods
+"""
+
+
 def lookup(obj):
+    """Lookup all attributes in my class
+
+    Args:
+        obj: object class
+
+    Returns:
+        returns the list of available attributes and methods of an object
+    """
     return [index for index in dir(obj)]
