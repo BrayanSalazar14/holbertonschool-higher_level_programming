62bf270c563d2549df8ee146c9f25ac36d1619e4
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-21 12:26:06 -0500

    new task V1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry whit one method
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry whit method area
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
