7a2e2fdd4b963a99f0e8742cb01b08a0a6231d41
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-21 15:59:17 -0500

    Changes for code and new task

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,6 +13,11 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
