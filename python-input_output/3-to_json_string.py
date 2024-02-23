7eddf978cfef8c772153dc59788c20ad0da9698f
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-23 13:28:57 -0500

    new task

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object (string) to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
