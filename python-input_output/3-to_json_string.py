0b2e61565652ad4110a0193ea8fc2d67d0d0308e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-23 13:31:46 -0500

    Docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""Module that returns the JSON representation of an object (string)
+"""
 import json
-"""
-Module that returns the JSON representation of an object (string)
-"""
 
 
 def to_json_string(my_obj):
