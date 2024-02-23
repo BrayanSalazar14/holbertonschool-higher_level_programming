e7e672f90c043695719b7810a4e1569966f5e141
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-23 12:07:40 -0500

    Documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Module that reads a text file (UTF8) and prints it to stdout
+"""
+
 
 def read_file(filename=""):
+    """
+     Read the content of a file and print it to the console.
+    Args:
+        filename (str, optional): The path of the file to be read.
+        If not provided, it will read from the standard input (stdin).
+    """
     with open(filename, "r", encoding="utf-8") as archivo:
         print(archivo.read(), end="")
