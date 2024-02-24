aa92c8ff621c6123437b8fdaef36d548a39a8fac
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-23 22:37:42 -0500

    new task

---
+++ python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Class Student that defines a student
+"""
+
+
+class Student:
+    """A class representing a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        my_dic = {}
+        for key, value in self.__dict__.items():
+            my_dic[key] = value
+        return my_dic
