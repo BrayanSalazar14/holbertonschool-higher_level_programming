fe75580801684e8f166f7a77e65c8431a8a89bcb
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-24 12:22:07 -0500

    Hot fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Module that returns a
+    list of lists of integers
+    representing the Pascal’s triangle of n
+"""
+
+
 def pascal_triangle(n):
-    """_summary_
+    """
+    function def pascal_triangle(n): that returns a
+    list of lists of integers
+    representing the Pascal’s triangle of n
 
     Args:
         n (int)
