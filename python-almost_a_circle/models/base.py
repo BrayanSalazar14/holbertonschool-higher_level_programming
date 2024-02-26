971997091d8411689b569b67469fb62cf9e435b8
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-26 12:23:14 -0500

    new proyect

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+"""
+Class will be the “base” of all other classes in this project
+"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
