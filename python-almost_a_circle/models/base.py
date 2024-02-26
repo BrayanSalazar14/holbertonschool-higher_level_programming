d6ccda67b20c6fef24141acaecd0705889d302ce
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-26 12:27:28 -0500

    check

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,11 @@ Class will be the “base” of all other classes in this project
 
 
 class Base:
+    """
+    Class Base, private class attribute __nb_objects = 0
+    if id is not None, assign the public instance
+    attribute id with this argument value
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
