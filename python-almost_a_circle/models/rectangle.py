0b321a9ded09db85257fdb794060a74757cef54e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-27 21:14:38 -0500

    changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""
-Module that class Rectangle
-"""
+"""Module for Rectangle class."""
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Class Rectangle
-
-    Args:
-        Base (class parent)
-    """
+    """Rectangle class that inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
