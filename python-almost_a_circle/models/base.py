fddd4be3824d29ca2faf839f09d19b2e56db4f40
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-28 09:12:14 -0500

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
