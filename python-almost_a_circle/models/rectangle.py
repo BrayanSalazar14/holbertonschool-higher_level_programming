538f60d569101c7d38d87053094e050ae3c51739
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-27 21:51:21 -0500

    hot fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Module for Rectangle class."""
+"""
+Module for Rectangle class.
+"""
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class that inherits from Base."""
+    """
+    Rectangle class that inherits from Base.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Module for Square class."""
+"""
+Module for Square class.
+"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Rectangle class that inherits from Rectangle."""
+    """
+    Rectangle class that inherits from Rectangle.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
