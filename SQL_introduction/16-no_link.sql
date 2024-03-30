46cc7d50a91c3f4d4893644fb4fa4d3a5dd779e5
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-30 00:22:19 -0500

    changes for the script

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
 --  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-INSERT INTO second_table(score, name) VALUES(18, "Aria");
-INSERT INTO second_table(score, name) VALUES(12, "Aria");
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
