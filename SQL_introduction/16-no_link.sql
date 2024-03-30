2227f9359bd45f3508c4f16f3a8749516ce86799
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-30 00:14:20 -0500

    Documentation

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
+--  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 INSERT INTO second_table(score, name) VALUES(18, "Aria");
 INSERT INTO second_table(score, name) VALUES(12, "Aria");
 SELECT score, name FROM second_table ORDER BY score DESC;
