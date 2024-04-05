1c4b7c7c5152485871fd11091694afd52dc4fe62
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-04 19:44:38 -0500

    Pycodestyle

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     instance = session.query(State).order_by(State.id).first()
     if instance is None:
         print("Nothing")
